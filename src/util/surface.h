9911378b161b04848253ba24e9bfb7924dae0ac1
Luke San Antonio <lukesanantonio@gmail.com>
2014-11-23 11:29:53 -0500

    Added Texture_Deleter and Texture_Ptr which are analogous to Surface_Deleter and Surface_Ptr.

--- src/util/surface.h
+++ src/util/surface.h
@@ -14,6 +14,14 @@ namespace game
       SDL_FreeSurface(surface);
     }
   };
-
   using Surface_Ptr = std::unique_ptr<SDL_Surface, Surface_Deleter>;
+
+  struct Texture_Deleter
+  {
+    void operator()(SDL_Texture* texture) noexcept
+    {
+      SDL_DestroyTexture(texture);
+    }
+  };
+  using Texture_Ptr = std::unique_ptr<SDL_Texture, Texture_Deleter>;
 }
