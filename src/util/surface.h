6a60cade992e16d606a301439a3087fd067c2f22
Luke San Antonio <lukesanantonio@gmail.com>
2014-11-24 16:10:36 -0500

    Added two functions to aid in the creation of shared_ptrs to surfaces and textures.

--- src/util/surface.h
+++ src/util/surface.h
@@ -16,6 +16,13 @@ namespace game
   };
   using Surface_Ptr = std::unique_ptr<SDL_Surface, Surface_Deleter>;
 
+  using Shared_Surface_Ptr = std::shared_ptr<SDL_Surface>;
+  inline Shared_Surface_Ptr
+  make_shared_surface(Shared_Surface_Ptr::element_type* ptr = nullptr) noexcept
+  {
+    return Shared_Surface_Ptr(ptr, Surface_Deleter{});
+  }
+
   struct Texture_Deleter
   {
     void operator()(SDL_Texture* texture) noexcept
@@ -24,4 +31,11 @@ namespace game
     }
   };
   using Texture_Ptr = std::unique_ptr<SDL_Texture, Texture_Deleter>;
+
+  using Shared_Texture_Ptr = std::shared_ptr<SDL_Texture>;
+  inline Shared_Texture_Ptr
+  make_shared_surface(Shared_Texture_Ptr::element_type* ptr = nullptr) noexcept
+  {
+    return Shared_Texture_Ptr(ptr, Texture_Deleter{});
+  }
 }
