13dea1a1b31d5c8660231800cb928269fef2d613
Luke San Antonio <lukesanantonio@gmail.com>
2014-12-17 19:14:00 -0500

    New unfog function that stores a makeship cache of positions that need to be cleared.

--- src/Player.cpp
+++ src/Player.cpp
@@ -29,8 +29,61 @@ namespace game
     }
   }
 
+  std::vector<Vec<int> > gen_pixels(double view_radius) noexcept
+  {
+    std::vector<Vec<int> > points;
+
+    const auto right_vec = Vec<double>{view_radius, 0};
+
+    // Clear the player position (relative to player, so 0,0 works).
+    points.push_back({0, 0});
+
+    // Circumference of the field of view.
+    auto circum = int(std::round(2 * PI * view_radius));
+    for(auto rot_step = 0; rot_step < circum; ++rot_step)
+    {
+      auto endpt = rotate<double>(right_vec, (2 * PI / circum) * rot_step);
+
+      // Since we already cleared the player position we know that at the very
+      // least, the endpoints of the circle (center + border) will be changed
+      // to the correct color. All of the other points in between will be
+      // easier to get to now that we know we don't have to worry about the
+      // extremes.
+      points.push_back(endpt);
+
+      auto endpt_length = length(endpt);
+      for(auto ray_len = 0.0; ray_len < endpt_length;)
+      {
+        // Find our position on the ray from the center to the outside of the
+        // circle.
+        Vec<int> inter_pt = normalize(endpt) * ray_len;
+
+        // Increment our position on the ray a little bit.
+        ray_len = std::min(endpt_length, ray_len + .5);
+
+        // Only add the point if it wasn't added last iteration, since that
+        // means we haven't actually moved pixels at all.
+        if(points.back() == inter_pt) continue;
+
+        // Clear the pixel and record the point last cleared.
+        points.push_back(inter_pt);
+      }
+    }
+
+    return points;
+  }
+
   void unfog(Player& p) noexcept
   {
+    static auto view_radius = 0;
+    static auto pixels = std::vector<Vec<int> >{};
+
+    if(view_radius != p.view_radius)
+    {
+      view_radius = p.view_radius;
+      pixels = gen_pixels(view_radius);
+    }
+
     // Sketch a the border of a circle than do a flood fill.
     if(!p.fog.surface()) return;
 
@@ -56,44 +109,9 @@ namespace game
       *pixel_ptr = clear_color;
     };
 
-    const auto right_vec = Vec<double>{p.view_radius, 0};
-
-    // Clear the player position (relative to player, so 0,0 works).
-    auto last_pt = Vec<int>{0,0};
-    clear_pixel(last_pt);
-
-    // Circumference of the field of view.
-    auto circum = int(std::round(2 * PI * p.view_radius));
-    for(auto rot_step = 0; rot_step < circum; ++rot_step)
+    for(auto const& pt : pixels)
     {
-      auto endpt = rotate<double>(right_vec, (2 * PI / circum) * rot_step);
-
-      // Since we already cleared the player position we know that at the very
-      // least, the endpoints of the circle (center + border) will be changed
-      // to the correct color. All of the other points in between will be
-      // easier to get to now that we know we don't have to worry about the
-      // extremes.
-      clear_pixel(endpt);
-      last_pt = endpt;
-
-      auto endpt_length = length(endpt);
-      for(auto ray_len = 0.0; ray_len < endpt_length;)
-      {
-        // Find our position on the ray from the center to the outside of the
-        // circle.
-        Vec<int> inter_pt = normalize(endpt) * ray_len;
-
-        // Increment our position on the ray a little bit.
-        ray_len = std::min(endpt_length, ray_len + .5);
-
-        // Only add the point if it wasn't added last iteration, since that
-        // means we haven't actually moved pixels at all.
-        if(last_pt == inter_pt) continue;
-
-        // Clear the pixel and record the point last cleared.
-        clear_pixel(inter_pt);
-        last_pt = inter_pt;
-      }
+      clear_pixel(pt);
     }
   }
 }
