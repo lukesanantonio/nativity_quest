ff4656a5249d8a0a95a9d7e75763c374433305c6
Luke San Antonio <lukesanantonio@gmail.com>
2014-12-24 23:36:01 -0500

    The Navigate_State now renders the map.

--- src/views/navigate.cpp
+++ src/views/navigate.cpp
@@ -3,6 +3,7 @@
  * All rights reserved.
  */
 #include "navigate.h"
+#include "../common/volume.h"
 namespace game
 {
   void Navigate_State::handle_event(SDL_Event const&) noexcept
@@ -13,5 +14,16 @@ namespace game
   }
   void Navigate_State::render() const noexcept
   {
+    auto& active_player = map.players[player];
+
+    auto viewport_src = view_pt<int>(game_.graphics.size(), map.size(),
+                                     active_player.pos, map.scale);
+
+    auto map_sprite = sprites.get_sprite(map.map_sprite);
+
+    auto viewport_src_rect = to_sdl_rect(viewport_src);
+    SDL_RenderCopy(game_.graphics.renderer,
+                   map_sprite->texture(game_.graphics.renderer),
+                   &viewport_src_rect, NULL);
   }
 }
