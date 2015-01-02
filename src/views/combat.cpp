49738267dcdd9679dc7dac7ad53ca7232e16b711
Luke San Antonio <lukesanantonio@gmail.com>
2015-01-02 00:15:55 -0500

    Random damage done to both the player and enemy, running randomly succeeds, etc.

--- src/views/combat.cpp
+++ src/views/combat.cpp
@@ -3,6 +3,7 @@
  * All rights reserved.
  */
 #include "combat.h"
+#include "../decl/combat.h"
 namespace game
 {
   Combat_State::Combat_State(Game& g, Navigate_State& ns,
@@ -35,6 +36,8 @@ namespace game
 
     enemy_bar.max = enemy.entity_data.max_life;
     player_bar.max = active_player().entity_data.max_life;
+
+    fight_state = Fight_State::Player_Turn;
   }
 
   void Combat_State::handle_event(SDL_Event const&) noexcept
@@ -42,6 +45,41 @@ namespace game
   }
   void Combat_State::step() noexcept
   {
+    if(active_player().entity_data.cur_life == 0 ||
+       enemy.entity_data.cur_life == 0)
+    {
+      pop_state(game_);
+    }
+
+    auto& label_view = boost::get<ui::Label_View>(hud.elements.back().element);
+    if(label_view.done && label_view.selected == 0 &&
+       fight_state == Fight_State::Player_Turn)
+    {
+      apply_damage(enemy.entity_data, decl::damage());
+      fight_state = Fight_State::Enemy_Turn;
+    }
+    else if(label_view.done && label_view.selected == 2 &&
+            fight_state == Fight_State::Player_Turn)
+    {
+      std::mt19937 prng{random_device()};
+
+      std::uniform_int_distribution<int> dist(0, 2);
+      if(dist(prng) == 0)
+      {
+        pop_state(game_);
+      }
+      else
+      {
+        fight_state = Fight_State::Enemy_Turn;
+      }
+    }
+    else if(fight_state == Fight_State::Enemy_Turn)
+    {
+      apply_damage(active_player().entity_data, decl::damage());
+      fight_state = Fight_State::Player_Turn;
+      label_view.done = false;
+    }
+
     auto& enemy_bar = boost::get<ui::Bar>(hud.elements[2].element);
     enemy_bar.cur = enemy.entity_data.cur_life;
 
--- src/views/combat.h
+++ src/views/combat.h
@@ -7,8 +7,19 @@
 #include "../Enemy_Instance.h"
 
 #include "../ui/Model.h"
+
+#include <random>
 namespace game
 {
+  enum class Fight_State
+  {
+    Enemy_Won,
+    Player_Won,
+    Player_Turn,
+    Enemy_Turn,
+    Running
+  };
+
   struct Combat_State : public Navigate_Sub_State
   {
     Combat_State(Game& g, Navigate_State& ns, Enemy_Instance& e) noexcept;
@@ -23,6 +34,10 @@ namespace game
     ui::Model hud;
 
     Enemy_Instance& enemy;
+
+    Fight_State fight_state;
+
+    std::random_device random_device;
 
     inline Player& active_player() noexcept;
     inline Player const& active_player() const noexcept;
