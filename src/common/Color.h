8d12e4027d3ee8c80c2a7c73dc92a6dbb2b1091c
Luke San Antonio <lukesanantonio@gmail.com>
2015-01-18 03:02:28 -0500

    Added equality and inequality operators for the Color structure.

--- src/common/Color.h
+++ src/common/Color.h
@@ -14,6 +14,15 @@ namespace game
     component_t blue;
   };
 
+  bool operator==(Color const& c1, Color const& c2) noexcept
+  {
+    return c1.red == c2.red && c1.green == c2.green && c1.blue == c2.blue;
+  }
+  bool operator!=(Color const& c1, Color const& c2) noexcept
+  {
+    return !(c1 == c2);
+  }
+
   void set_render_draw_color(SDL_Renderer* renderer, Color c) noexcept;
 
   inline SDL_Color to_sdl_color(Color c) noexcept
