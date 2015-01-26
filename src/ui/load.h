cd1594935a6bbbe83a6c50359b032844d76e9046
Luke San Antonio <lukesanantonio@gmail.com>
2015-01-26 16:12:59 -0500

    Added a load_as function that applies a cast to the view returned by the corrosponding load function.

--- src/ui/load.h
+++ src/ui/load.h
@@ -6,9 +6,17 @@
 #include <string>
 #include "../game/Game.h"
 
+#include "cast.h"
 #include "View.h"
 namespace game { namespace ui
 {
   struct Bad_Orientation{};
+
   Shared_View load(Game&, std::string file) noexcept;
+
+  template <typename T>
+  inline std::shared_ptr<T> load_as(Game& g, std::string file) noexcept
+  {
+    return as<T>(load(g, file));
+  }
 } }
