c1b2cf7747d0dd758f6808626320d4a307b7306d
Luke San Antonio <lukesanantonio@gmail.com>
2015-02-04 17:13:13 -0500

    Added a new view that just takes up space.

---
+++ src/ui/views/empty.h
@@ -0,0 +1,31 @@
+/*
+ * Copyright (C) 2014 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+#include "../View.h"
+namespace game { namespace ui
+{
+  /*!
+   * \brief A view that simply takes up space.
+   */
+  struct Empty : public View
+  {
+    inline Vec<int> get_minimum_extents() const noexcept override;
+    inline void dispatch_event(SDL_Event const&) noexcept override;
+    inline Volume<int> layout_() override;
+    inline void render_() const noexcept override;
+  };
+  inline Vec<int> Empty::get_minimum_extents() const noexcept
+  {
+    return {0, 0};
+  }
+  inline void Empty::dispatch_event(SDL_Event const&) noexcept
+  { }
+  inline Volume<int> Empty::layout_()
+  {
+    return parent_volume();
+  }
+  inline void Empty::render_() const noexcept
+  { }
+} }
