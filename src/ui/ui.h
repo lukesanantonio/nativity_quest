6cf12d560458eb09ba251eb0730e35bd2e7dcb9b
Luke San Antonio <lukesanantonio@gmail.com>
2015-02-14 19:59:51 -0500

    Added a header to include all things related to the UI.

---
+++ src/ui/ui.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2014 Luke San Antonio
+ * All rights reserved.
+ */
+
+#include "layouts/grid_layout.h"
+#include "layouts/linear_layout.h"
+#include "layouts/side_layout.h"
+
+#include "views/bar.h"
+#include "views/empty.h"
+#include "views/label.h"
+#include "views/sprite.h"
+
+#include "cast.h"
+#include "load.h"
+#include "View.h"
+#include "View_Container.h"
