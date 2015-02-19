f681cfe235fa9eac99b9ef77384234ea6d72295b
Luke San Antonio <lukesanantonio@gmail.com>
2015-02-18 19:52:51 -0500

    Revert "New zone hack that makes it work on Mac os x."
    This reverts commit ab91a75d153e30f4197b3d5c592a4c5d618d859f.

--- src/decl/zones.cpp
+++ src/decl/zones.cpp
@@ -7,30 +7,10 @@
 #include <cstdint>
 #include "SDL.h"
 
-#include "../common/json.h"
-
 namespace game { namespace decl
 {
   Zone Zones::get_zone(Vec<int> pos) const noexcept
   {
-    static std::vector<std::vector<short> > buttons;
-
-    if(buttons.empty())
-    {
-        auto doc = parse_json("assets/zones.json");
-
-        for(auto row_iter = doc.Begin(); row_iter != doc.End(); ++row_iter)
-        {
-            buttons.emplace_back();
-            for(auto col_iter = row_iter->Begin();
-                col_iter != row_iter->End();
-                col_iter += 3)
-            {
-                buttons.back().push_back(col_iter->GetInt());
-            }
-        }
-    }
-
     SDL_Surface* png = sprite_->surface();
     if(!png) return no::zone;
 
@@ -38,12 +18,18 @@ namespace game { namespace decl
     if(pos.x < 0 || pos.y < 0) return no::zone;
     if(pos.x > png->w || pos.y > png->h) return no::zone;
 
+    // pixel represents the red component of the color at pixel pos.
+    uint8_t* pixel = (uint8_t*) png->pixels;
+    pixel += (pos.y * png->pitch) +
+             (pos.x * png->format->BytesPerPixel);
+    if(!pixel) return no::zone;
+
     // Find the zone that is represented by the same color as our pixel.
     using std::begin; using std::end;
     auto zone_find = std::find_if(begin(zones_), end(zones_),
-    [&pos](auto zone)
+    [&pixel](auto zone)
     {
-      return buttons[pos.y][pos.x] == zone->color;
+      return *pixel == zone->color;
     });
 
     if(zone_find == end(zones_))
@@ -52,6 +38,5 @@ namespace game { namespace decl
     }
 
     return *zone_find;
-
   }
 } }
