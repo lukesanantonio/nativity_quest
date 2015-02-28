86ceb8de1e0ac8385fa0a7931fee194163cdbf06
Luke San Antonio <lukesanantonio@gmail.com>
2015-02-28 12:10:16 -0500

    Documented the assets::discover function.

--- src/assets/discovery.h
+++ src/assets/discovery.h
@@ -7,5 +7,11 @@
 #include <string>
 namespace game { namespace assets
 {
+  /*!
+   * \brief Scans a directory for any files.
+   *
+   * \returns A vector of filepaths relative to the subdirectory passed into
+   * the function.
+   */
   auto discover(std::string subdir) noexcept -> std::vector<std::string>;
 } }
