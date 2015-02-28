de522cd9acc400cfd524e9526e9e24ced7882c57
Luke San Antonio <lukesanantonio@gmail.com>
2015-02-28 12:19:10 -0500

    Added a header for the assets/ subtree that includes all the useful files.

---
+++ src/assets/assets.h
@@ -0,0 +1,12 @@
+/*
+ * Copyright (C) 2014 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+
+#include "desc/game.h"
+#include "asset.h"
+#include "describe.h"
+#include "discovery.h"
+#include "find.h"
+#include "load.h"
