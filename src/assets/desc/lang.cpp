ea99f93a6fe0c405b8cb87c0e3b12d7ece5bc044
Luke San Antonio <lukesanantonio@gmail.com>
2015-02-28 12:51:36 -0500

    Added a language json interpreter (describe function implementation).

---
+++ src/assets/desc/lang.cpp
@@ -0,0 +1,19 @@
+/*
+ * Copyright (C) 2014 Luke San Antonio
+ * All rights reserved.
+ */
+#include "lang.h"
+namespace game { namespace assets
+{
+  void describe(Asset_Ptr<Lang> const& json, Lang& lang) noexcept
+  {
+    auto const& doc = json->json;
+
+    for(auto iter = doc.MemberBegin(); iter != doc.MemberEnd(); ++iter)
+    {
+      auto key = std::string{iter->name.GetString()};
+      lang.dict[key] = iter->value.GetString();
+    }
+  }
+} }
+
---
+++ src/assets/desc/lang.h
@@ -0,0 +1,18 @@
+/*
+ * Copyright (C) 2014 Luke San Antonio
+ * All rights reserved.
+ */
+#pragma once
+#include "../describe.h"
+#include <unordered_map>
+namespace game { namespace assets
+{
+  struct Lang
+  {
+    using asset_t = Json_Asset;
+
+    std::unordered_map<std::string, std::string> dict;
+  };
+
+  void describe(Asset_Ptr<Lang> const&, Lang&) noexcept;
+} }
