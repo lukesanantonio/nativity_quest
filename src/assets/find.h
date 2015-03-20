93f3a2f9b231304e004a823a5280617ef0998f05
Luke San Antonio <lukesanantonio@gmail.com>
2015-03-20 19:33:55 -0400

    The assets::find function now accepts a reference/const reference to assets.
    Before it was making a copy of the vector. Now we have two versions of the
    function that work with const and nonconst assets.

--- src/assets/find.h
+++ src/assets/find.h
@@ -11,8 +11,8 @@
 #include "asset.h"
 namespace game { namespace assets
 {
-  template <class T> inline std::shared_ptr<T>
-  find(assets::Vector assets, std::string name) noexcept
+  template <class T> inline std::shared_ptr<const T>
+  find(assets::Vector const& assets, std::string name) noexcept
   {
     using std::begin; using std::end;
     auto asset_find = std::find_if(begin(assets), end(assets),
@@ -24,5 +24,12 @@ namespace game { namespace assets
     if(asset_find == end(assets)) return nullptr;
 
     return std::dynamic_pointer_cast<T>(*asset_find);
+  }
+
+  template <class T> inline std::shared_ptr<T>
+  find(assets::Vector& assets, std::string name) noexcept
+  {
+    return std::const_pointer_cast<T>(find<T>(
+      const_cast<assets::Vector const&>(assets), name));
   }
 } }
